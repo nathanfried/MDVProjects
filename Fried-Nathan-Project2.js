00bca7d4f10b4f1c052459319b8a44c7bec1b028
nathanfried <freedom28@fullsail.com>
2013-04-19 03:14:21 -0300

    Update Fried-Nathan-Project2.js
    Needed adjustments for Returns

--- Fried-Nathan-Project2.js
+++ Fried-Nathan-Project2.js
@@ -8,35 +8,45 @@
 // My Variables
 var toyCowboy = "Woody";
 var toyRanger = "Buzz";
-var numberOfMinutes = [ 10, 15, 20, 12 ];
+var numberOfMinutes = [ 10, 20, 30, 40 ];
 var kids = [ "Bobby", "Jimmy", "Kate", "Sarah" ];
 
+
 // Array Function
-for (var i=0; i < 4; i++) {
-    console.log( kids[i] + " played with " + toyCowboy + " and " + toyRanger + " for " + numberOfMinutes[i] + " minutes.")
-}
-
-// Number Function
-var getTimesPickedUp = function (name, b, w ) {
-    var pickedUp = b + w;
-    console.log(name + " picked up Buzz " + b + " times and Woody " + w + " times, making it a total of " + pickedUp + ".")
-}
-
-// Boolean Function
-var getGoodTime = function () {
-    var goodTime = 30
-    if ((goodTime >= 30 && goodTime <= 50)) {
-        console.log("The children who played with " + toyCowboy + " and " + toyRanger + " for " + goodTime + " minutes or more, enjoyed their playtime.")
-    } else {
-        console.log("The children who had less time with the toys, enjoyed playtime more.")
+var getPlayTime = function () {
+    for (var i=0; i < 4; i++) {
+    console.log( kids[i] + " played with " + toyCowboy + " and " + toyRanger + " for " + numberOfMinutes[i] + " minutes.");
     }
 }
 
 
+// Number Function
+var getTimesPickedUp = function ( b, w ) {
+    var pickedUp = b + w;
+    return pickedUp
+}
+
+
+// Boolean Function
+var getGoodTime = function () {
+    var goodTime = 30;
+    if ((goodTime >= 30 && goodTime <= 50)) {
+        return "The children who had more time with the toys, enjoyed their playtime.";
+    } else {
+        return "The children who had less time with the toys, enjoyed playtime more.";
+    }
+}
+
+
+// String Function
+
+
+
+
+
 // My Functions
 getPlayTime();
-getTimesPickedUp("Bobby", 13, 18);
-getTimesPickedUp("Jimmy", 23, 7);
-getTimesPickedUp("Kate", 30, 14);
-getTimesPickedUp("Sarah", 4, 9);
-
+var myReturn = getTimesPickedUp( 20, 30 );
+console.log("The average amount of times " + toyCowboy + " and " + toyRanger + " were held was " + myReturn + ".");
+var myReturn = getGoodTime();
+console.log(myReturn)
