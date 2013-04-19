f6191fb948211f81587331fe443bea16107bf2b3
nathanfried <freedom28@fullsail.com>
2013-04-18 23:37:10 -0300

    Update Fried-Nathan-Project2.js
    Adding the Number Function

--- Fried-Nathan-Project2.js
+++ Fried-Nathan-Project2.js
@@ -15,3 +15,20 @@ var kids = [ "Bobby", "Jimmy", "Kate", "Sarah" ];
 for (var i=0; i < 4; i++) {
     console.log( kids[i] + " played with " + toyCowboy + " and " + toyRanger + " for " + numberOfMinutes[i] + " minutes.")
 }
+
+// Number Function
+var getTimesPickedUp = function (name, b, w ) {
+    var pickedUp = b + w;
+    console.log(name + " picked up Buzz " + b + " times and Woody " + w + " times, making it a total of " + pickedUp + ".")
+}
+
+//Boolean Function
+
+
+// My Functions
+getPlayTime();
+getTimesPickedUp("Bobby", 13, 18);
+getTimesPickedUp("Jimmy", 23, 7);
+getTimesPickedUp("Kate", 30, 14);
+getTimesPickedUp("Sarah", 4, 9);
+
