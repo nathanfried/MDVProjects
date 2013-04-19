c04bb91a5b99b95491190cff94d238c7b8d2901b
nathanfried <freedom28@fullsail.com>
2013-04-18 22:55:47 -0300

    Update Fried-Nathan-Project2.js
    First development of story.

--- Fried-Nathan-Project2.js
+++ Fried-Nathan-Project2.js
@@ -1,2 +1,17 @@
 //alert("JavaScript works!");
 
+// Nathan Fried
+// SDI 1304
+// Project 2
+
+
+// My Variables
+var toyCowboy = "Woody";
+var toyRanger = "Buzz";
+var numberOfKids = 4;
+var kidsPlayedWith = ["Bobby", "Jimmy", "Kate", "Sarah"];
+
+// Array Function
+for (var i=0; i < 4; i++) {
+    console.log( kids[i] + " played with " + toyCowboy + " and " + toyRanger + ".")
+}
