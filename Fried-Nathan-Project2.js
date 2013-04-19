5b5f3b7814620d25557c71e4f4bf46a63e04fd35
nathanfried <freedom28@fullsail.com>
2013-04-19 16:22:36 -0300

    Update Fried-Nathan-Project2.js
    String function added

--- Fried-Nathan-Project2.js
+++ Fried-Nathan-Project2.js
@@ -14,7 +14,7 @@ var kids = [ "Bobby", "Jimmy", "Kate", "Sarah" ];
 
 // Procedure
 var toyStory = function () {
-    console.log( kids[0] + ", " + kids[1] + ", " + kids[2] + ", and " + kids[3] + " all played with " + toyCowboy + " and " + toyRanger + ".");
+    console.log( kids[0] + ", " + kids[1] + ", " + kids[2] + ", and " + kids[3] + " all played with " + toyCowboy + " and " + toyRanger + " during the movie, Toy Story.");
 }
 
 
@@ -34,9 +34,9 @@ var getTotalToys = function (toys) {
         toys = toys - 1
         
         if (toys === toys) {
-            console.log("But if each child only had " + toys + " they were upset, and could not act out the movie.");
+            console.log("But if each child only had " + toys + " they were upset, because they wanted both.");
         } else {
-            console.log("They began to get upset.");
+            console.log("They didn't have any toy to play with.");
         }
     }
     return 2
@@ -46,17 +46,23 @@ var getTotalToys = function (toys) {
 var getGoodTime = function () {
     var goodTime = 30;
     if ((goodTime >= 30 && goodTime <= 50)) {
-        return "The children who had more time with the toys, enjoyed their playtime.";
+        return "The children who had more time with the toys, didn't watch the movie as much.";
     } else {
-        return "The children who had less time with the toys, enjoyed playtime more.";
+        return "The children who had less time with the toys, watched the movie more.";
     }
 }
 
 
 // String Function
-
-
-
+var saySpaceMotto = function() {
+    var motto1 = "To inifinity"
+    var motto2 = "and beyond!"
+    if (motto1 && motto2) {
+        return("The children were shouting " + motto1 + " " + motto2 + " by the end of the movie.");
+    } else{
+        return("The children forgot " + toyRanger + "'s motto was.");
+    }
+}
 
 
 // My Functions
@@ -64,6 +70,8 @@ toyStory(130);
 var totalKids = getPlayTime();
 console.log("The total amount of kids that played with the toys was " + totalKids + ".");
 var totalToys = getTotalToys(2);
-console.log("Each child was able to play with " + totalToys + " today.");
+console.log("Each child was able to play with at least " + totalToys + " today.");
 var enjoyPlay = getGoodTime();
 console.log(enjoyPlay);
+var buzzMotto = saySpaceMotto()
+console.log(buzzMotto)
