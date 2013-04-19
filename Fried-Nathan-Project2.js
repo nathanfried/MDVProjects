adf75f7defe84b4aeab33e33eb89618d155e49d2
nathanfried <freedom28@fullsail.com>
2013-04-18 23:03:13 -0300

    Update Fried-Nathan-Project2.js
    Adjustments to Array

--- Fried-Nathan-Project2.js
+++ Fried-Nathan-Project2.js
@@ -8,10 +8,10 @@
 // My Variables
 var toyCowboy = "Woody";
 var toyRanger = "Buzz";
-var numberOfKids = 4;
-var kidsPlayedWith = ["Bobby", "Jimmy", "Kate", "Sarah"];
+var numberOfMinutes = [ 10, 15, 20, 12 ];
+var kids = [ "Bobby", "Jimmy", "Kate", "Sarah" ];
 
 // Array Function
 for (var i=0; i < 4; i++) {
-    console.log( kids[i] + " played with " + toyCowboy + " and " + toyRanger + ".")
+    console.log( kids[i] + " played with " + toyCowboy + " and " + toyRanger + " for " + numberOfMinutes[i] + " minutes.")
 }
