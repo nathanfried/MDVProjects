7f1cf0712a3d10c550ad7c9fc3f43f303453b597
nathanfried <freedom28@fullsail.edu>
2013-05-02 21:49:09 -0300

    Getting the groundwork in

--- Fried-Nathan-Project4.js
+++ Fried-Nathan-Project4.js
@@ -5,3 +5,31 @@
 // Project 4
 // Library of Functions
 
+var jsLibrary = function(){
+    
+    // CHECK PHONE NUMBER
+    var phoneNumber = function(val){
+        if (val.length === 12) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+
+    // RETURNS
+    return {
+        
+        "phoneNumber": phoneNumber,
+
+
+// Function Calls
+var myLib = new jsLibrary();
+
+
+// Adjusted Variables
+var myNumber = "419-296-8884";
+
+
+// String Tests
+console.log("There are enough numbers to dial so it should be " + myLib.phoneNumber(myNumber));
