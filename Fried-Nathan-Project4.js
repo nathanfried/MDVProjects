27779df92aa8ee10caa49ec60837c1ce2845d1c4
nathanfried <freedom28@fullsail.edu>
2013-05-02 21:52:57 -0300

    Added URL

--- Fried-Nathan-Project4.js
+++ Fried-Nathan-Project4.js
@@ -25,6 +25,14 @@ var jsLibrary = function(){
         }
     }
 
+        // CHECK URL
+    var URL = function(val){
+        if (val.indexOf(":") === 4 || 5) {
+            return true
+        } else {
+            return false
+        }
+    }
 
 
     // RETURNS
@@ -32,7 +40,7 @@ var jsLibrary = function(){
         
         "phoneNumber": phoneNumber,
         "email": eMail,
-        
+        "URL": URL,
     };
     
 }
@@ -45,7 +53,15 @@ var myLib = new jsLibrary();
 // Adjusted Variables
 var myNumber = "419-296-8884";
 var myEmail = "freedom28@gmail.com";
+var myUrl = "http://www.google.com";
+
 
 // String Tests
 console.log("There are enough numbers to dial so it should be " + myLib.phoneNumber(myNumber));
 console.log("This email should be " + myLib.email(myEmail));
+console.log("This URL address should be " + myLib.URL(myUrl));
+
+
+
+
+
